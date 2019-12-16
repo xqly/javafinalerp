2670a6829a1c1d5163a746498be257cbba399d2c
12123 <404930714@qq.com>
2019-12-16 19:33:05 +0800

    add some controller

--- src/main/java/com/example/javafinalerp/Controller/MaterialsManageController.java
+++ src/main/java/com/example/javafinalerp/Controller/MaterialsManageController.java
@@ -1,8 +1,13 @@
 package com.example.javafinalerp.Controller;
 
+import com.example.javafinalerp.Bean.MHouseLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MaterialsManageController {
@@ -15,8 +20,16 @@ public class MaterialsManageController {
         return "materials_manage/materialDestroy";
     }
 
+    @RequestMapping("add_materials")
+    public String amt(@RequestParam("materials_json") String x){
+        return "redirect:materials_in_stock";
+    }
+
     @RequestMapping("materials_in_stock")
-    public String mis(){
+    public String mis(Model model)
+    {
+        List<MHouseLog> lists =null;
+        model.addAttribute("lists",lists);
         return "materials_manage/materials_in_stock";
     }
 
