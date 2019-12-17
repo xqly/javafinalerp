b8793722a6a3b5cfc62d06c0fd492dbdc80adfce
wu <510744622@qq.com>
2019-12-17 13:34:17 +0800

    生产计划

--- src/main/java/com/example/javafinalerp/Controller/ProductManageController.java
+++ src/main/java/com/example/javafinalerp/Controller/ProductManageController.java
@@ -1,9 +1,14 @@
 package com.example.javafinalerp.Controller;
 
 
+import com.example.javafinalerp.Bean.ProducePlan;
+import com.example.javafinalerp.tempclass.Materialsandname;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductManageController {
@@ -22,7 +27,9 @@ public class ProductManageController {
     }
 
     @RequestMapping("producePlan")
-    public String pdp(){
+    public String pdp(Model model){
+        List<ProducePlan> lists=null;
+        model.addAttribute("lists",lists);
         return "product_manage/producePlan";
     }
 
