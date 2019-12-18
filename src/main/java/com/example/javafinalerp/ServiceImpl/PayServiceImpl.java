56ea9ccea5e6c91e16e7b786aa568c3613836a18
12123 <404930714@qq.com>
2019-12-19 02:51:06 +0800

    add some

--- src/main/java/com/example/javafinalerp/ServiceImpl/PayServiceImpl.java
+++ src/main/java/com/example/javafinalerp/ServiceImpl/PayServiceImpl.java
@@ -1,30 +1,36 @@
 package com.example.javafinalerp.ServiceImpl;
 
 import com.example.javafinalerp.Bean.PayList;
+import com.example.javafinalerp.Resitory.PayListResitory;
 import com.example.javafinalerp.Service.PayService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PayServiceImpl implements PayService {
+
+    @Autowired
+    PayListResitory payListResitory;
+
     @Override
     public List<PayList> getlistbytype(Integer x, Integer y) {
-        return null;
+        return payListResitory.getlistbytype(x,y);
     }
 
     @Override
     public List<PayList> getlistbypid(Integer id, Integer x, Integer y) {
-        return null;
+        return payListResitory.getlistbypid(x,y,id);
     }
 
     @Override
     public List<PayList> getlistbyoid(Integer id, Integer x, Integer y) {
-        return null;
+        return payListResitory.getlistbyoid(x,y,id);
     }
 
     @Override
     public List<PayList> getlistbyman(Integer man, Integer x, Integer y) {
-        return null;
+        return payListResitory.getlistbytypeman(x,y,man);
     }
 }
