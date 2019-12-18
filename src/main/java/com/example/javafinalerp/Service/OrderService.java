e4816973fa471a8ff3b86f53563467a659779545
北纬 <13287005649@163.com>
2019-12-18 23:19:11 +0800

    仓库管理controller

---
+++ src/main/java/com/example/javafinalerp/Service/OrderService.java
@@ -0,0 +1,7 @@
+package com.example.javafinalerp.Service;
+import com.example.javafinalerp.Bean.Order;
+import java.util.List;
+
+public interface OrderService {
+    List<Order> getWHLList();//获取所有
+}
