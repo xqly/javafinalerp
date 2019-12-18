b44f3c2138b0dbd4d39ffe086c616d3b26216c61
12123 <404930714@qq.com>
2019-12-18 15:54:04 +0800

    fix some bugs

--- src/main/java/com/example/javafinalerp/Service/ProductManageService.java
+++
@@ -1,9 +0,0 @@
-package com.example.javafinalerp.Service;
-
-import com.example.javafinalerp.Bean.ProducePlan;
-
-import java.util.List;
-
-public interface ProductManageService {
-    List<ProducePlan> getproduct();
-}
