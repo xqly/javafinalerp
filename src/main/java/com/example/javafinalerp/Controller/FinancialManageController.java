2e5c8f467437d32d1b6ab2d4e80ff5519c8adc90
12123 <404930714@qq.com>
2019-12-19 03:44:29 +0800

    add money

--- src/main/java/com/example/javafinalerp/Controller/FinancialManageController.java
+++ src/main/java/com/example/javafinalerp/Controller/FinancialManageController.java
@@ -1,29 +1,42 @@
 package com.example.javafinalerp.Controller;
 
 
+import com.example.javafinalerp.Bean.PayList;
 import com.example.javafinalerp.Bean.User;
+import com.example.javafinalerp.Service.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class FinancialManageController {
+
+    @Resource
+    PayService payService;
 
     @Autowired
     FinancialManageController financialManageController;
 
     @RequestMapping("account_in")
-    public String acin(){
+    public String acin(Model model){
+        List<PayList> payLists = payService.getlistbytype(1,1);
+        model.addAttribute("lists",payLists);
         return "financial_manage/account_in";
     }
 
     @RequestMapping("advance_money_in")
-    public String admi(){
+    public String admi(Model model)
+    {
+        List<PayList> payLists = payService.getlistbytype(2,1);
+        model.addAttribute("lists",payLists);
         return "financial_manage/advance_money_in";
     }
 
@@ -32,7 +45,10 @@ public class FinancialManageController {
         return "financial_manage/money_in";
     }
     @RequestMapping("money_out")
-    public String Mo(){
+    public String Mo(Model model)
+    {
+        List<PayList> payLists = payService.getlistbytype(1,2);
+        model.addAttribute("lists",payLists);
         return "financial_manage/money_out";
     }
 
