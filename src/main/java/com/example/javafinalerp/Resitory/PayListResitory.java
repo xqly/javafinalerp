cd87004cda7dfedcde23a5820771424d9b2426dd
12123 <404930714@qq.com>
2019-12-19 02:03:50 +0800

    add jpa

---
+++ src/main/java/com/example/javafinalerp/Resitory/PayListResitory.java
@@ -0,0 +1,7 @@
+package com.example.javafinalerp.Resitory;
+
+import com.example.javafinalerp.Bean.PayList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayListResitory extends JpaRepository<PayList,Integer> {
+}
