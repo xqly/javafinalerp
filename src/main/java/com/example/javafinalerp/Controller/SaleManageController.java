be3bad9637a744af76f64fcb601123c94813a473
wu <510744622@qq.com>
2019-12-19 03:57:17 +0800

    订单

--- src/main/java/com/example/javafinalerp/Controller/SaleManageController.java
+++ src/main/java/com/example/javafinalerp/Controller/SaleManageController.java
@@ -2,16 +2,19 @@ package com.example.javafinalerp.Controller;
 
 import com.example.javafinalerp.Bean.Goods;
 import com.example.javafinalerp.Bean.Ordergoods;
+import com.example.javafinalerp.Bean.OW;
 import com.example.javafinalerp.Bean.WHouseLog;
 import com.example.javafinalerp.Service.BasicManageService;
 import com.example.javafinalerp.Service.OrderService;
+import com.example.javafinalerp.Service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import com.example.javafinalerp.tempclass.Orderandname;
+import com.example.javafinalerp.tempclass.OWname;
 import javax.annotation.Resource;
 import java.util.List;
 
@@ -21,6 +24,8 @@ public class SaleManageController {
     BasicManageService basicManageService;
     @Resource
     OrderService orderService;
+    @Resource
+    Orderandname orderandname;
     @Autowired
     SaleManageController saleManageController;
 
@@ -33,12 +38,25 @@ public class SaleManageController {
 
     @RequestMapping("now__sale")
     public String ns(Model model){
-        List<Ordergoods> order=orderService.getorderlist();
+        List<Orderandname> order=null;
         List<Goods> goods=basicManageService.getgoodslist();
         model.addAttribute("order",order);
         model.addAttribute("goods",goods);
         return "sale_manage/now__sale";
     }
+
+    @RequestMapping("tiaozhuan")
+    public String ns4(){
+        return "sale_manage/children";
+    }
+
+    @RequestMapping(value = "select_by_oid",method = RequestMethod.POST)
+    public String crk2(@RequestParam("uid") Integer id,  Model model){
+        List<OWname> goods1 = null;
+        model.addAttribute("goods1",goods1);
+        return "sale_manage/children";
+    }
+
 
     @RequestMapping(value = "query_by_oid",method = RequestMethod.POST)
     public String crk(@RequestParam("OID") Integer id,  Model model){
