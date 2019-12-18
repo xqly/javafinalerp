8d8f9693b7de029a025f0ffad5758f4dc3fcd764
12123 <404930714@qq.com>
2019-12-19 02:01:33 +0800

    add pay

--- src/main/java/com/example/javafinalerp/Service/PayService.java
+++ src/main/java/com/example/javafinalerp/Service/PayService.java
@@ -1,6 +1,13 @@
 package com.example.javafinalerp.Service;
 
 
-public interface PayService {
+import com.example.javafinalerp.Bean.PayList;
 
+import java.util.List;
+
+public interface PayService {
+    List<PayList> getlistbytype(Integer x,Integer y);
+    List<PayList> getlistbypid(Integer id,Integer x,Integer y);
+    List<PayList> getlistbyoid(Integer id,Integer x,Integer y);
+    List<PayList> getlistbyman(Integer man,Integer x,Integer y);
 }
