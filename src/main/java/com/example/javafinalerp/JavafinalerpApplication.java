2d67c449d1fbba0e13dd4ba611fe3c1252de7e8b
北纬 <13287005649@163.com>
2019-12-20 00:57:08 +0800

    基本信息部分页面-添加增删改接口

--- src/main/java/com/example/javafinalerp/JavafinalerpApplication.java
+++ src/main/java/com/example/javafinalerp/JavafinalerpApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavafinalerpApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JavafinalerpApplication.class, args);
     }
-
 }
