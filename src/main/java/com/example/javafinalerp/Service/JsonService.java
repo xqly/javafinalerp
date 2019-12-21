07f1317ffa3e241672be09e81692da2408d30577
12123 <404930714@qq.com>
2019-12-21 15:01:10 +0800

    add json service

---
+++ src/main/java/com/example/javafinalerp/Service/JsonService.java
@@ -0,0 +1,11 @@
+package com.example.javafinalerp.Service;
+
+import com.alibaba.fastjson.JSONArray;
+
+public interface JsonService {
+    JSONArray Material();
+    JSONArray MHouse();
+    JSONArray Goods();
+    JSONArray Workshop();
+
+}
---
+++ src/main/java/com/example/javafinalerp/ServiceImpl/JsonServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.javafinalerp.ServiceImpl;
+
+import com.alibaba.fastjson.JSONArray;
+import com.example.javafinalerp.Service.JsonService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonServiceImpl implements JsonService {
+    @Override
+    public JSONArray Material() {
+        return null;
+    }
+
+    @Override
+    public JSONArray MHouse() {
+        return null;
+    }
+
+    @Override
+    public JSONArray Goods() {
+        return null;
+    }
+
+    @Override
+    public JSONArray Workshop() {
+        return null;
+    }
+}
